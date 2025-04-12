0c5a407ac290bda5cea5dbddbbe74e4c80a65e84
Mintu Sikder <mintusikder15@gmail.com>
2025-04-12 20:47:45 +0600

    added tailwind react price option

---
+++ tailwind-react-pricing-option/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="navbar bg-base-100 shadow-sm">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    </>
+  );
+}
+
+export default App;
