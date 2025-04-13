174609075fa295960b7e290ccca7b54311fc19f3
Mintu Sikder <mintusikder15@gmail.com>
2025-04-13 09:12:07 +0600

    added membership plan

--- tailwind-react-pricing-option/src/App.jsx
+++ tailwind-react-pricing-option/src/App.jsx
@@ -1,9 +1,15 @@
+import { Suspense } from "react";
 import NavBar from "./components/NavBar/NavBar";
+import PricingOption from "./components/PricingOption/PricingOption";
 
 function App() {
+  const pricingPromise = fetch("pricing.json").then((res) => res.json());
   return (
     <>
       <NavBar></NavBar>
+      <Suspense fallback={<p>Loading...</p>}>
+        <PricingOption pricingPromise={pricingPromise}></PricingOption>
+      </Suspense>
     </>
   );
 }
--- tailwind-react-pricing-option/src/components/PricingOption/PricingOption.jsx
+++ tailwind-react-pricing-option/src/components/PricingOption/PricingOption.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { use } from "react";
+import PriceCard from "../PriceCard/PriceCard";
 
-const PricingOption = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PricingOption = ({ pricingPromise }) => {
+  const pricing = use(pricingPromise);
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto mt-12">
+        {pricing.map((prices) => (
+          <PriceCard key={prices.id} prices={prices}></PriceCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PricingOption;
