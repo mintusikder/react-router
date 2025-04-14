9d9df452f02dd336c7ca8ac2abecf0ce7511f5f1
Mintu Sikder <mintusikder15@gmail.com>
2025-04-14 13:01:58 +0600

    added navLink

---
+++ intro-react-router/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>React router footer</small></p>
+        </div>
+    );
+};
+
+export default Footer;
--- intro-react-router/src/components/Header/Header.jsx
+++ intro-react-router/src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import {  NavLink } from 'react-router';
+import "./Header.css"
 const Header = () => {
     return (
         <div>
             <h2>This is header</h2>
+            <nav className='header'>
+               <NavLink to="/">Home</NavLink>
+               <NavLink to="/mobile">Mobile</NavLink>
+            </nav>
         </div>
     );
 };
