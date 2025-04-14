6a20dc54afed60d4a1ac2d0736a201964e92b8e8
Mintu Sikder <mintusikder15@gmail.com>
2025-04-14 10:36:35 +0600

    added react routing

--- intro-react-router/src/main.jsx
+++ intro-react-router/src/main.jsx
@@ -3,11 +3,32 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import Mobile from "./components/Mobile/Mobile.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "mobile",
+        Component: Mobile,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element: <div>Hello About</div>,
+  },
+  {
+    path: "/app",
+    Component: App,
   },
 ]);
 
